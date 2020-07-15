6a97a11b5d199c3242c624f1b3b58a450b05b2cb
meaghan jennings <meaghanljennings@gmail.com>
2020-07-15 22:05:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,12 +7,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  add_element_to_end_of_array << "marker"
-  puts add_element_to_end_of_array.inspect
+  add_element_to_end_of_array << "color"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  add_element_to_start_of_array.unshift("token")  
 end
 
 def remove_element_from_end_of_array(array)
